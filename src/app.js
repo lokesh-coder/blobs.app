5e8ed4a70ff46cf28c1b902c2832604e2b4f481a
udaypydi <udaypydi333@gmail.com>
2020-04-06 23:59:07 +0530

    eslint fixes

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class App extends React.Component {
-    render() {
-        return (
-            <div>Welcome to React Boilerplate!</div>
-        );
-    }
-}
