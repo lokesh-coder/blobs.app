8420477dc999522c18183cd4aec3780f085300c1
udaypydi <udaypydi333@gmail.com>
2020-04-14 09:36:37 +0530

    fix test cases after adding css and sass loader

--- jest.config.js
+++ jest.config.js
@@ -83,7 +83,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy"
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
