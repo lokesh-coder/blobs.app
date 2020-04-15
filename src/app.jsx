1faf70aa34997931db54670c6b42be082ca5310c
udaypydi <udaypydi333@gmail.com>
2020-04-15 08:44:25 +0530

    landing page changed

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import './app.scss';
 export default function App() {
   return (
     <div className="App--Container">
+      <p className="App--Title">Welcome To React Boilerplate</p>
       <img src={logo} className="App--Image" />
     </div>
   );
