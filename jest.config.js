ecd8bad14a5b094e3dc84da5227a004701aff043
udaypydi <udaypydi333@gmail.com>
2020-04-15 08:56:57 +0530

    fix test cases

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/config/assetsMock.js",
     "\\.(css|less|scss)$": "identity-obj-proxy"
   },
   testEnvironment: "jsdom",
