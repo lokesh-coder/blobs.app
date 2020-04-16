787c805680e4e854302168657ad88dee14edf1f9
udaypydi <udaypydi333@gmail.com>
2020-04-16 22:40:08 +0530

    app.jsx module resolver fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import Loader from 'uielements/loader/loader.component';
-const App = React.lazy(() => import('./src/app.jsx'));
+const App = React.lazy(() => import('./src/app'));
 
 ReactDOM.render(<Suspense fallback={<Loader />}><App /></Suspense>, document.getElementById('root'));
