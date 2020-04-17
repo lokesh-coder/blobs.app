5b78ec8715633ae3369cfdff0349f14c217f2947
udaypydi <udaypydi333@gmail.com>
2020-04-17 23:06:52 +0530

    module aliasing added in jest config

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
   coverageDirectory: "coverage",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/config/assetsMock.js",
-    "\\.(css|less|scss)$": "identity-obj-proxy"
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "^uielements(.*)$": "<rootDir>/src/uielements$1"
   },
   testEnvironment: "jsdom",
 };
--- src/uielements/__tests__/loader.spec.js
+++ src/uielements/__tests__/loader.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'test-utils';
-import  Loader from '../loader/loader.component.jsx';
+import  Loader from 'uielements/loader/loader.component.jsx';
 
 it('should take a snapshot', () => {
     const { asFragment } = render(<Loader />)
