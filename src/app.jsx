75ee51580bee726ba458b5a460a36b29f978addc
udaypydi <udaypydi333@gmail.com>
2020-04-23 14:13:21 +0530

    eslint fixes for unresolved module(imports)

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.png';
 import Button from 'uielements/button/button.component';
+import logo from './logo.png';
 import 'assets/css/main.css';
 import './app.scss';
-
 
 export default function App() {
   return (
     <div className="bg-blue-900 flex flex-col justify-center items-center p-20">
-        <p className="text-4xl text-blue-100">Welcom To React Boilerplate!</p>
-        <img src={logo} className="app--image" />
-        <Button  buttonText="Get Started!" />
+      <p className="text-4xl text-blue-100">Welcom To React Boilerplate!</p>
+      <img src={logo} className="app--image" alt="logo" />
+      <Button buttonText="Get Started!" />
     </div>
   );
 }
