2ac1fc29d0ba45fa3eb0b18b2f7437c9b732ab5b
lokesh-coder <mexican.dirtyfellow@gmail.com>
2020-06-02 06:27:27 +0530

    add button to download svg

--- src/components/actions/svgCopy.jsx
+++ src/components/actions/svgCopy.jsx
@@ -5,9 +5,11 @@ import { appStore } from "../../store";
 import Modal from "../common/modal";
 import Highlight from "../common/highlight";
 import Copy from "../common/ copy";
+import Download from "../common/download";
 
 const SVGCopy = view(({ onClose }) => {
   const [isModalOpen, openModal] = useState(false);
+  const ID = `${appStore.edges}-${appStore.growth}-${appStore.id}`;
   const code = `
   <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
     <path d="${appStore.path}"></path>
@@ -26,6 +28,7 @@ const SVGCopy = view(({ onClose }) => {
       </Button>
       <Modal isOpen={isModalOpen} onClose={() => openModal(false)}>
         <div className="flex justify-end">
+          <Download content={code} filename={`blob_${ID}.svg`} />
           <Copy text={code} />
         </div>
         <Highlight code={code} lang={"markup"} />
